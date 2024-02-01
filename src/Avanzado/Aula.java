156c12ef7a06c923416f7ad633dc84b2a740d92e
Admin <Admin@LAPTOP-O93FSHCL>
2024-02-01 23:25:43 +0100

    Se crea un metodo add, para añadir alumnos

--- src/Avanzado/Aula.java
+++ src/Avanzado/Aula.java
@@ -4,35 +4,26 @@ import java.util.Arrays;
 
 public class Aula {
 
-	
 	private Alumno[] alumnos;
 	private int contador;
-	
+
 	public Aula() {
-		alumnos=new Alumno[5];
+		alumnos = new Alumno[5];
 	}
+
 	public void setAlumnos(Alumno[] alumnos) {
 		this.alumnos = alumnos;
 	}
+
 	public void add(Alumno alumno) {
-		if(contador<alumnos.length) {
-			alumnos[contador]= alumno;
+		if (contador < alumnos.length) {
+			alumnos[contador] = alumno;
 		}
-		
-		
 	}
-	
-	
-	
+
 	@Override
 	public String toString() {
 		return "Aula [alumnos=" + Arrays.toString(alumnos) + "]";
 	}
 
-
-	
-	
-	
-	
-	
 }
