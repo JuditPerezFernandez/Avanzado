c6db4b44207eb8f12fff7ceaf1448f9292103c1a
Admin <Admin@LAPTOP-O93FSHCL>
2024-02-01 23:36:34 +0100

    Dentro de la clase aula, creamos un aula, un alumno y una asignatura

---
+++ src/Avanzado/Start.java
@@ -0,0 +1,13 @@
+package Avanzado;
+
+public class Start {
+
+	public static void main(String[] args) {
+		Aula aula1=new Aula();
+		Alumno alumno=new Alumno("Juan","Fernández",123456789);
+		Asignatura asignatura1=new Asignatura("Lengua",8,4,9);
+		
+
+	}
+
+}
