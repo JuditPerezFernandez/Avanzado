d259b0f35cc41dd7d84f62c64dcb98be17684e72
Admin <Admin@LAPTOP-O93FSHCL>
2024-02-01 23:24:53 +0100

    Se crean los siguientes atributos; Un vector Alumnos que nos indica los alumnos que podemos añadir. Y un contador para no añadir mas de los 5 permitidos.

--- src/Avanzado/Aula.java
+++ src/Avanzado/Aula.java
@@ -1,8 +1,34 @@
 package Avanzado;
 
+import java.util.Arrays;
+
 public class Aula {
 
 	
+	private Alumno[] alumnos;
+	private int contador;
+	
+	public Aula() {
+		alumnos=new Alumno[5];
+	}
+	public void setAlumnos(Alumno[] alumnos) {
+		this.alumnos = alumnos;
+	}
+	public void add(Alumno alumno) {
+		if(contador<alumnos.length) {
+			alumnos[contador]= alumno;
+		}
+		
+		
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Aula [alumnos=" + Arrays.toString(alumnos) + "]";
+	}
+
 
 	
 	
