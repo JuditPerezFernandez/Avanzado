ce6eeca2bec8b38f8069921c70e72d9c8e0ffeff
Admin <Admin@LAPTOP-O93FSHCL>
2024-02-01 23:39:00 +0100

    Dentro del alumno añadimos una asignatura y dentro del aula añadimos el alumno.

--- src/Avanzado/Start.java
+++ src/Avanzado/Start.java
@@ -4,9 +4,10 @@ public class Start {
 
 	public static void main(String[] args) {
 		Aula aula1=new Aula();
-		Alumno alumno=new Alumno("Juan","Fernández",123456789);
+		Alumno alumno1=new Alumno("Juan","Fernández",123456789);
 		Asignatura asignatura1=new Asignatura("Lengua",8,4,9);
-		
+		alumno1.add(asignatura1);
+		aula1.add(alumno1);
 
 	}
 
